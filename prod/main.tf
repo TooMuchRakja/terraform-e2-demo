8a260cd8d4b861813f01337f4fc29960224e380f
TooMuchRakja <szymekp1994@gmail.com>
2025-05-25 13:51:07 +0200

    added prod

---
+++ prod/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_version "= 1.5.7"
+
+  backend "s3" {
+    bucket  = "terraform-kurs-wiaderko-199548"
+    key     = "13-github_actions/prod/terraform.tfstate"
+    region  = "eu-central-1"
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+module "compute" {
+  source        = "../moduły/main"
+  name          = var.name
+  ami           = var.ami
+  instance_type = var.instance_type
+  tags          = var.tags
+}
---
+++ prod/prod.tfvars
@@ -0,0 +1,7 @@
+name          = "ec2-prod"
+ami           = "ami-02b7d5b1e55a7b5f1"
+instance_type = "t2.micro"
+tags = {
+  Env = "prod"
+  Typ = "maszyna_prod"
+}
