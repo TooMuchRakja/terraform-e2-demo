e54cdad1cf8ed2509fe55cc28ba80990b090ef76
TooMuchRakja <szymekp1994@gmail.com>
2025-05-25 14:00:38 +0200

    poprawiona wersja terraform

--- prod/main.tf
+++ prod/main.tf
@@ -1,25 +1,23 @@
 terraform {
-  required_version "= 1.5.7"
+  required_version = "1.5.7"
 
   backend "s3" {
     bucket  = "terraform-kurs-wiaderko-199548"
-    key     = "13-github_actions/prod/terraform.tfstate"
+    key     = "14-github_actions/prod/terraform.tfstate"
     region  = "eu-central-1"
     encrypt = true
   }
-}
 
-provider "aws" {
-  region = var.region
-}
-
-terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
+}
+
+provider "aws" {
+  region = var.region
 }
 
 module "compute" {
